da0df1b53aa46b414bf0baf6aa73c98ef4621109
Laroikin <said24abdi04@gmail.com>
2024-09-27 12:00:31 +0900

    add auth and shadcn

--- client/web-frontend/src/app/page.tsx
+++ client/web-frontend/src/app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <></>;
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { auth } from '~/auth';
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <p>
+        {!session?.user
+          ? 'You are not logged in!'
+          : `Hello, ${session.user.name}`}
+      </p>
+      {!session?.user ? (
+        <Button asChild>
+          <Link href="/auth">Sign In</Link>
+        </Button>
+      ) : (
+        <Button asChild>
+          <Link href="/api/auth/signout">Sign Out</Link>
+        </Button>
+      )}
+    </>
+  );
 }
