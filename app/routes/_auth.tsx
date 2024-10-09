2d5b1bd5de213eea67563c3549d75cb5974255f1
Laroikin <said24abdi04@gmail.com>
2024-10-10 05:43:29 +0900

    add auth forms

---
+++ app/routes/_auth.tsx
@@ -0,0 +1,26 @@
+import { Outlet } from '@remix-run/react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '~/components/ui/card';
+
+export default function Page() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Outlet />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
