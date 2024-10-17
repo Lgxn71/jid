f13301f9ab8bbf527ab969b5074ae7f741e381a4
Mukhammedali Akhmetbekuly <muhanai1913@gmail.com>
2024-10-18 06:52:52 +0900

    resolved merge conflict

--- app/root.tsx
+++ app/root.tsx
@@ -17,19 +17,18 @@ import { themeSessionResolver } from "./sessions.server";
 import "~/tailwind.css";
 import { Toaster } from "./components/ui/toaster";
 
-
 import { authenticator } from "./auth.server";
 import { parse, stringify } from "superjson";
 import type { User } from "@prisma/client";
 import { UserContext } from "./context/userContext";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	const { getTheme } = await themeSessionResolver(request);
-	const user = await authenticator.isAuthenticated(request);
-	return {
-		theme: getTheme(),
-		user: stringify(user),
-	};
+  const { getTheme } = await themeSessionResolver(request);
+  const user = await authenticator.isAuthenticated(request);
+  return {
+    theme: getTheme(),
+    user: stringify(user),
+  };
 }
 
 export default function AppWithProviders() {
@@ -55,29 +54,28 @@ export const links: LinksFunction = () => [
 ];
 
 function Layout() {
-	const data = useLoaderData<typeof loader>();
-	const user = parse<User | null>(data.user);
-	const [theme] = useTheme();
+  const data = useLoaderData<typeof loader>();
+  const user = parse<User | null>(data.user);
+  const [theme] = useTheme();
 
-	return (
-		<html lang="en" className={clsx(theme)}>
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
-				<Links />
-			</head>
-			<body>
-			<Toaster />
+  return (
+    <html lang="en" className={clsx(theme)}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
+        <Links />
+      </head>
+      <body>
+        <Toaster />
 
-				<UserContext.Provider value={user}>
-					<Outlet />
-				</UserContext.Provider>
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	);
->>>>>>> main
+        <UserContext.Provider value={user}>
+          <Outlet />
+        </UserContext.Provider>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
